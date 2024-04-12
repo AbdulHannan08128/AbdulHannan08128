48948b81a8149a81fe5b9bf75b1f55d90e19ec55
AbdulHannan08128 <154576645+AbdulHannan08128@users.noreply.github.com>
2024-04-12 15:27:29 +0530

    V1-e

---
+++ app/(site)/test/page.js
@@ -0,0 +1,46 @@
+
+import { client } from '@/utils/configSanity';
+import React from 'react';
+import {urlForImage} from "@/utils/configSanity";
+import Image from 'next/image'
+export const dynamic = 'force-dynamic'
+async function getData() {
+  
+  const query = "*[_type == 'blog']";
+  const data = await client.fetch(query);
+  console.log();
+  return data;
+  
+}
+
+const Page = async () => {
+  const blogs = await getData();
+
+  return (
+    <div className='m-40'>
+      <div>
+        {blogs.map(blog => (
+          <div key={blog._id} className="border rounded-lg p-4 shadow-md mb-4">
+            <h2 className="text-xl font-semibold mb-2">{blog.title}</h2>
+            {blog.cover_image && blog.cover_image.asset && (
+             
+             <>
+             <img src={urlForImage(blog.cover_image).width(400).height(200).url()}alt={blog.title}/>
+             </>
+            )}
+            <p className="text-gray-600 mb-4">{new Date(blog.date_pub).toLocaleDateString()}</p>
+            <ul className="list-disc list-inside">
+              {blog.content.map((paragraph, index) => (
+                <li key={index} className="mb-2">
+                  {paragraph.text} 
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Page;
--- utils/configSanity.ts
+++ utils/configSanity.ts
@@ -1,4 +1,6 @@
 import {createClient} from 'next-sanity';
+import imageUrlBuilder from '@sanity/image-url';
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 export const client = createClient({
     projectId:'qhzy9ruh',
@@ -6,3 +8,9 @@ export const client = createClient({
     apiVersion:'2022-03-07',
     useCdn:true
 })
+
+const builder = imageUrlBuilder(client);
+
+export function urlForImage(source: SanityImageSource) {
+  return builder.image(source)
+}
