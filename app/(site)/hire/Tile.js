8669ca42971964b67e8aa8324f6206cb72f43bfa
AbdulHannan08128 <154576645+AbdulHannan08128@users.noreply.github.com>
2024-04-30 11:02:58 +0530

    a

--- app/(site)/hire/Tile.js
+++ app/(site)/hire/Tile.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const Tile = ({href, img}) => {
   return (
     <>
-     <div className="bg-gray-200 p-2 rounded-lg flex items-center justify-center w-30 h-30">
+     <div className="bg-gray-200 p-2 rounded-lg flex items-center justify-center w-30 h-30 hover:bg-gray-300 transition-all">
         <Link href={href}>
           
             <img src={img} alt="Tile 4" className="max-w-full max-h-full rounded-full" />
--- app/(site)/hire/page.js
+++ app/(site)/hire/page.js
@@ -1,18 +1,29 @@
 import React from 'react';
-
+import SectionHeader from '@/components/Common/SectionHeader';
 import Tile from './Tile'
-export const metadata ={
-    title:'Hire Abdul Hannan - Full Stack Web Developer'
+export const metadata = {
+  title: 'Hire Abdul Hannan - Full Stack Web Developer'
 }
 const Page = () => {
+
   return (
-    <div className="gap-4 mt-40 p-12 flex flex-wrap justify-center align-middle">
-        <Tile href={'https://www.upwork.com/freelancers/~017281d9f43346f4cd?mp_source=share'} img={'/logo-upwork.png'}/>
-        <Tile href={'https://www.fiverr.com/ab_hannan_web'} img={'/logo-fiverr.png'}/>
-        <Tile href={'https://www.freelancer.in/u/AbdulHannan08128'} img={'/logo-freelancer.jpeg'}/>
-        <Tile href={'https://www.linkedin.com/in/abdul-hannan-b44ab02a8'} img={'/linkedin-logo.png'}/>
-        <Tile href={'https://github.com/AbdulHannan08128'} img={'/github-logo.png'}/>
-        <Tile href={'/support'} img={'/email-logo.jpeg'}/>
+    <div className=' mt-28'>
+    <SectionHeader
+    headerInfo={{
+      title: `Hire Me`,
+      subtitle: `Get Started`,
+      description: ``,
+    }}
+  />
+    <div className="gap-4 p-12 flex flex-wrap justify-center align-middle ">
+     
+      <Tile href={'https://www.upwork.com/freelancers/~017281d9f43346f4cd?mp_source=share'} img={'/logo-upwork.png'} />
+      <Tile href={'https://www.fiverr.com/ab_hannan_web'} img={'/logo-fiverr.png'} />
+      <Tile href={'https://www.freelancer.in/u/AbdulHannan08128'} img={'/logo-freelancer.jpeg'} />
+      <Tile href={'https://www.linkedin.com/in/abdul-hannan-b44ab02a8'} img={'/linkedin-logo.png'} />
+      <Tile href={'https://github.com/AbdulHannan08128'} img={'/github-logo.png'} />
+      <Tile href={'/support'} img={'/email-logo.jpeg'} />
+    </div>
     </div>
   );
 };
